ab88d04dadda4cb4f4b303048436a8125a7a4ede
Katia-Radchenko <82673991+Katia-Radchenko@users.noreply.github.com>
2021-10-30 23:13:39 +0200

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -12,7 +12,7 @@ ingredients.forEach(option => {
   const lastItem = document.createElement('li');
   lastItem.textContent = option;
   lastItem.classList.add('item');
-  navEl.append(lastItem)
+  navEl.append( lastItem)
   console.log(lastItem)
 })
 
